8b36352086d9ebf89563665d0cf26d92168aacac
Marcelo Almeida <marcelo@mraweb.com.br>
2014-05-14 16:27:14 -0300

    atualizacao

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8" />
+<title></title>
+<!--<link rel="stylesheet" type="text/css" href="css/style.css">-->
+<!--[if ie]>
+	<script type="text/javascript" src="js/html5-ie.js"></script>
+<![endif]-->
+<?php include "analytics.php"; ?>
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+
+<div class="bg-home">
+    <header role="banner">
+        <h1><a href="/" title="" class="hidetxt"></a></h1>
+        
+        <nav role="navigation">
+            <ul>
+                <li><a href="/" title="Home" class="btHome">Home</a></li>
+                <li><a href="informacoes.php" title="Informações" class="btInfo">Informações</a></li>
+                <li><a href="empresa.php" title="Empresa" class="btEmpr">Empresa</a></li>
+                <li><a href="cotacao.php" title="Cotação" class="btCota">Cotação</a></li>
+                <li><a href="servicos.php" title="Serviços" class="btServ">Serviços</a></li>
+                <li><a href="contato.php" title="Contato" class="btCont">Contato</a></li>
+            </ul>
+        </nav>
+    </header><!-- FINAL HEADER -->
+    
+    <section id="content" role="main">
+    
+    </section><!-- FINAL CONTENT -->  
+</div>
+
+<div class="bg-rodape">
+    <footer role="contentinfo">
+    
+    </footer>
+</div>
+<script type="text/javascript" src="js/jquery-1.7.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
